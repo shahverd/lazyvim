3f66b8276a7366ba132c915bcbc801ad8804cd48
Mostafa Shahverdy <shahverdy.m@gmail.com>
2024-07-19 16:12:57 +0330

    Added Flog

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd([[colorscheme tokyonight-day]])
+vim.cmd([[colorscheme tokyonight]])
 vim.keymap.set("n", "<F9>", "<Cmd>Neotree toggle<CR>")
 vim.cmd([[cd ~]])
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-python"),
+  },
+})
