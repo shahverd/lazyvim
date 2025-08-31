5373fcbbe89298d15ffab8265068115123cfacef
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-08-31 04:48:04 +0330

    debug for c++

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,30 +1,65 @@
 return {
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            "mfussenegger/nvim-dap-python",
-            "nvim-neotest/nvim-nio",
-        },
-        config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui", -- UI for dap
+			"williamboman/mason.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
 
-            dapui.setup()
+			-- Setup dap-ui
+			dapui.setup()
 
-            -- Auto open/close dap-ui
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
+			-- Open/close UI automatically
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
 
-            -- Python setup
-            require("dap-python").setup("python")
-        end,
-    },
+			dap.adapters.cppdbg = {
+				id = "cppdbg",
+				type = "executable",
+				command = vim.fn.stdpath("data") .. "/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+			}
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch C++ file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						-- Path to your executable (adjust as needed)
+						local exe = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/main", "file")
+
+						-- Run make synchronously so we know if it failed
+						local result = vim.fn.system("make")
+
+						if vim.v.shell_error ~= 0 then
+							-- Show errors in a scratch buffer
+							vim.cmd("new")
+							vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(result, "\n"))
+							vim.notify("Build failed. Debugger not started.", vim.log.levels.ERROR)
+							return nil -- 🚫 returning nil cancels debug start
+						end
+
+						return exe
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+				},
+			}
+
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
+		end,
+	},
 }
