9458bca3ed94718aa72fd051c1c780404f1e91e5
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-09-18 05:33:10 +0330

    C++ debug:  debug.lua

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,65 +1,60 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui", -- UI for dap
-			"williamboman/mason.nvim",
-			"jay-babu/mason-nvim-dap.nvim",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-
-			-- Setup dap-ui
-			dapui.setup()
-
-			-- Open/close UI automatically
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-
-			dap.adapters.cppdbg = {
-				id = "cppdbg",
-				type = "executable",
-				command = vim.fn.stdpath("data") .. "/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
-			}
-
-			dap.configurations.cpp = {
-				{
-					name = "Launch C++ file",
-					type = "codelldb",
-					request = "launch",
-					program = function()
-						-- Path to your executable (adjust as needed)
-						local exe = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/main", "file")
-
-						-- Run make synchronously so we know if it failed
-						local result = vim.fn.system("make")
-
-						if vim.v.shell_error ~= 0 then
-							-- Show errors in a scratch buffer
-							vim.cmd("new")
-							vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(result, "\n"))
-							vim.notify("Build failed. Debugger not started.", vim.log.levels.ERROR)
-							return nil -- 🚫 returning nil cancels debug start
-						end
-
-						return exe
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-				},
-			}
-
-			dap.configurations.c = dap.configurations.cpp
-			dap.configurations.rust = dap.configurations.cpp
-		end,
-	},
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "williamboman/mason.nvim",
+            "jay-babu/mason-nvim-dap.nvim",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            -- Setup Mason DAP to automatically install adapters
+            require("mason-nvim-dap").setup({
+                ensure_installed = { "codelldb" }, -- Install codelldb for C++
+                automatic_installation = true,
+            })
+            -- Setup DAP UI
+            dapui.setup()
+            -- Auto-open/close DAP UI
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            -- Adapter configuration for codelldb
+            dap.adapters.codelldb = {
+                type = "executable",
+                command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+                name = "codelldb",
+            }
+            -- C++-specific configuration
+            dap.configurations.cpp = {
+                {
+                    name = "Launch C++ file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        -- Prompt user for the executable path
+                        -- local executable = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                        local executable = vim.fn.getcwd() .. "/bin/main"
+                        if executable == "" then
+                            vim.notify("No executable specified. Debugger not started.", vim.log.levels.ERROR)
+                            return nil
+                        end
+                        return executable
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    args = function()
+                        -- return vim.split(vim.fn.input("Arguments: "), "%s+")
+                    end,
+                },
+            }
+            -- Keybindings for stopping the debuger
+            vim.keymap.set("n", "<leader>dx", function()
+                dap.terminate()
+                dapui.close()
+            end, { desc = "Debug: Terminate" })
+        end,
+    },
 }
