39093d0c3c83e80b557e141bbdce10d42ad6e1c3
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-09-18 05:36:16 +0330

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,12 @@
 local dap = require("dap")
 
 -- Start/continue
-vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue/Start Debugging" })
+vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue/Start Debugging" })]
+vim.keymap.set("n", "<leader>dx", function()
+    dap.terminate()
+    dapui.close()
+end, { desc = "Debug: Terminate" })
+
 -- Step controls
 vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
 vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
