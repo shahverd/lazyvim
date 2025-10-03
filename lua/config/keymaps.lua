b0325064dc9fffb61b65f163bed38ed312e63bd4
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-10-03 09:46:17 +0330

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,15 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 --
 -- Debugging keymaps
 local dap = require("dap")
+local dapui = require("dapui")
 
 -- Start/continue
 vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue/Start Debugging" })
 vim.keymap.set("n", "<leader>dx", function()
-    dap.terminate()
-    dapui.close()
+  dap.terminate()
+  dapui.close()
 end, { desc = "Debug: Terminate" })
 
 -- Step controls
@@ -20,12 +24,12 @@ vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Step Out" })
 -- Breakpoints
 vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
 vim.keymap.set("n", "<leader>dB", function()
-    dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+  dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 end, { desc = "Set Conditional Breakpoint" })
 
 -- Debug UI (dap-ui)
 vim.keymap.set("n", "<leader>dd", function()
-    require("dapui").toggle()
+  require("dapui").toggle()
 end, { desc = "Toggle Debugger UI" })
 
 -- REPL
