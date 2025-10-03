cc8a7a5761f12146d244fedef292a2c0cef9edd2
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-10-03 09:45:52 +0330

    Update snacks.lua

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,10 +19,10 @@ return {
       },
       sections = {
         { section = "header" },
-        { section = "keys", padding = 1, indent = 2, title = "Bookmarks" },
+        -- { section = "keys", padding = 1, indent = 2, title = "Bookmarks" },
         { section = "projects", padding = 1, indent = 2, title = "Recebt Projects" },
-
-        { section = "terminal", pane = 2, cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+        --
+        --        { section = "terminal", pane = 2, cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
         { section = "recent_files", pane = 2, limit = 8, padding = 1, indent = 2, title = "Recent Files" },
       },
     },
